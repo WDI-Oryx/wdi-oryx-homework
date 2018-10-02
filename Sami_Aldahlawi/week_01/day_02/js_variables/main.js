0f2c64917ed1b4a4b31379968d4317df0797eba0
MacBookPro <MacBookPro@MacBookPros-MacBook-Po.local>
2018-10-02 13:38:08 +0300

    add js_variables folder

---
+++ Sami_Aldahlawi/week_01/day_02/js_variables/main.js
@@ -0,0 +1,41 @@
+//The Fortune Teller
+    /*Why pay a fortune teller when you can just program your fortune yourself?
+    Store the following into variables: number of children, partner's name, geographic location, job title. Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+    */
+
+
+
+
+    //The Age Calculator
+
+var currentAge = 2018;
+var birthYear = 1995;
+var calAge = currentAge - birthYear;
+console.log("They are either " + calAge );
+
+
+    //The Lifetime Supply Calculator
+var age = 22;
+var maxAge = 65;
+var amount = 1;
+var restEatFood = (maxAge - age) * amount  * 360;
+console.log("You will need "+ restEatFood +" to last you until the ripe old age of "+ maxAge);
+
+
+
+    //The Geometrizer
+var radius = 2;
+var  Pi = 3.14;
+var circumference = radius * 2 * Pi;
+console.log("The area is  "+ circumference );
+
+
+    //The Temperature Converter
+var celsiusT = 22;
+var fahrenheit = 72;
+var calCtoF = celsiusT* 1.8 + 32;
+var calFtoC = (fahrenheit - 32) * .5556;
+console.log(celsiusT+" °C is "+calCtoF+"°F");
+console.log(fahrenheit+" °F is "+calFtoC+"°C");
+
+

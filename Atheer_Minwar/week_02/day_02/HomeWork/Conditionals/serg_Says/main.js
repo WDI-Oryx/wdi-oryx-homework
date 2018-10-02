fd7ead2e7e035afdd670ee26434a8985f83650c9
atheer 10 <atheer.m.070@gmail.com>
2018-10-02 10:19:58 +0300

    homework files

---
+++ Atheer_Minwar/week_02/day_02/HomeWork/Conditionals/serg_Says/main.js
@@ -0,0 +1,9 @@
+var userMessage = "hmm" ;
+if (userMessage.endsWith ("?")) { 
+    console.log("Sure");
+} else if (userMessage === userMessage.toLocaleUpperCase ) { 
+    console.log("Woah,chill out!");
+} else if (userMessage === null || userMessage === "" ) {
+    console.log("Fine.Be that way!")
+} else (console.log("Whatever")); 
+
---
+++ Atheer_Minwar/week_02/day_02/HomeWork/Variables/lifetime_Supply_Calculator/main.js
@@ -0,0 +1,7 @@
+var currentAge = 15;
+var maxAge = 30;
+var amountPerDay = 4;
+var HumanlifeSpan = 79;
+var consumption = (HumanlifeSpan * amountPerDay) * (maxAge - currentAge);
+
+console.log("You will need " + consumption + " to last you until the ripe old age of " + HumanlifeSpan);

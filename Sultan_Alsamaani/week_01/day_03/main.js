5d8275481e30ba3a2fdcc19a43ea7effaa5f5064
sultan2001b <sultan2001b@gmail.com>
2018-10-03 09:08:23 +0300

    home work w1 d3

---
+++ Sultan_Alsamaani/week_01/day_03/main.js
@@ -0,0 +1,26 @@
+function squareNumber(number) {
+    return number*number;
+}
+
+function halfNumber(number) {
+    return number/2;
+}
+
+function percentOf(num1,num2) {
+    if (num1>num2) {
+        return (num2/num1)*100+"%";
+    } else {
+        return (num1/num2)*100+"%";
+    }
+}
+function areaOfCircle(num) {
+    return 3.14159*squareNumber(num);
+}
+
+function part2(num) {
+    var result = halfNumber(num);
+    result = squareNumber(result);
+    result = areaOfCircle(result);
+    result = percentOf(result,squareNumber(result));
+    return result;
+}

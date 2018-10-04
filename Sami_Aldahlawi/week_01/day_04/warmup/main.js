793769dd412c26788debfd94037f1b97b02f3bc8
wh0-1-am <o.t5@hotmail.com>
2018-10-04 09:53:03 +0300

    HW day_04

--- Sami_Aldahlawi/week_01/day_04/warmup/main.js
+++ Sami_Aldahlawi/week_01/day_04/warmup/main.js
@@ -3,7 +3,6 @@ console.log("js connected!");
 var j = 0;
 var carCompanies = ['Honda', 'Toyota', 'Ford', 'Hyundai', 'Mazda', 'Hummer'];
 while (j < carCompanies.length) {
-    
     if (carCompanies[j].startsWith('H')) {
         console.log(carCompanies[j]);
     }

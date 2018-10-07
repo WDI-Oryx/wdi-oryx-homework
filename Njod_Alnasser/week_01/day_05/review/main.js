bb0ef564bf264b7b453a907b73c3169800e971e6
YOUR GITHUB USERNAME HERE <njod@njods-MacBook-Air.local>
2018-10-07 08:48:00 +0300

    homework day 5

---
+++ Njod_Alnasser/week_01/day_05/review/main.js
@@ -0,0 +1,42 @@
+console.log("hello");
+
+var emptyArray=[];
+var letters=["A","B","C","D","E"];
+//accessing element 
+
+var firstletter=letters[0];
+
+console.clear();
+
+for (var i= 0; i<=10; i++){
+console.log(i);
+}
+
+var music=[ "gitar", "piano",  "vilon","sas"];
+for ( var i=0; i<4 ;i+=2){
+    var currentmusic=music[i];
+    console.log(currentmusic);
+}
+
+var movies={
+title:"kjj",
+dirctor:"grv",
+duration:423
+};
+
+console.log(movies);
+
+var bookSeries = {
+    name: "In Search of Lost Time",
+    author: "Marcel Proust",
+    books: [
+      "Swann's Way",
+      "In the Shadow of Young Girls in Flower",
+      "The Guermantes Way",
+      "Sodom and Gomorrah",
+      "The Prisoner",
+      "The Fugitive",
+      "Time Regained"
+    ]
+  };
+  console.log(bookSeries);

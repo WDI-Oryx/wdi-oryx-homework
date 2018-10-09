70a8b1ea812452fed937db17e443750e2e6d3b47
Moath <mfalthaw@ucalgary.ca>
2018-10-09 10:21:02 +0300

    fix caps

--- _Warmup_Exercises/week_02/day_03/main.js
+++ _Warmup_Exercises/week_02/day_03/main.js
@@ -1,20 +1,20 @@
 // Planet to Earth years
 var planets = {
-    "earth": 1,
-    "mercury": 0.2408467,
-    "venus": 0.61519726,
-    "mars": 1.8808158,
-    "jupiter": 11.862615,
-    "saturn": 29.447498,
-    "uranus": 84.016846,
-    "neptune": 164.79132,
+    "Earth": 1,
+    "Mercury": 0.2408467,
+    "Venus": 0.61519726,
+    "Mars": 1.8808158,
+    "Jupiter": 11.862615,
+    "Saturn": 29.447498,
+    "Uranus": 84.016846,
+    "Neptune": 164.79132,
 };
 
 var yearInSeconds = 31557600;
 var ageInSeconds = 0;
 
 function convert(timeInSeconds, toPlanet) {
-    return convertedAge = timeInSeconds / yearInSeconds * planets[toPlanet.toLowerCase()];
+    return convertedAge = timeInSeconds / yearInSeconds * planets[toPlanet];
 }
 
 function convertAndLog(seconds) {

b0800a14d701bc2a6b21f6eaf159ae1e19446e14
leanaAlq <leana.a.q@gmail.com>
2018-10-10 14:29:17 +0300

    ignore

--- Leana_Alqurashi/week_01/day_05/MTA_main.js
+++ Leana_Alqurashi/week_01/day_05/MTA_main.js
@@ -1,13 +1,13 @@
 var stops = 0; // Stops Counter
 var index = 0, index2 = 0; // keep track of index
-debugger;
-var Trip = []; //user's input saved in an array
+var Trip = ["L", "L-8th", "N", "N-8th"]; //user's input saved in an array
 var path = []; // trip path saved in array
 var uS = "Union Square";
 //lines
 var LineN = ["Times Square", "34th", "28th", "23rd", uS, "N-8th"];
 var LineL = ["L-8th", "6th", uS, "L-3rd", "1st"];
 var LineSix = ["Grand Central", "33rd", "6-8th", "6-3rd", uS, "Astor Place"];
+/*
 function FunctionOne() {
     var a = document.getElementById("first").value;
     Trip.push(a);
@@ -27,31 +27,31 @@ function FunctionFour() {
     var d = document.getElementById("fourth").value;
 
     Trip.push(d);
-    search(Trip);
 
+    search(Trip);
 }
+
 document.getElementById("first").onchange = FunctionOne;
 document.getElementById("second").onchange = FunctionTwo;
 document.getElementById("third").onchange = FunctionThree;
 document.getElementById("fourth").onchange = FunctionFour;
-
+*/
 //document.getElementById("showStop").innerHTML = "<br>" + "you will stop " + stops + " times" + "<br>";
 //document.getElementById("showPath").innerHTML = "Your path is: " + path + "<br>";
 
 
 
+search(Trip);
 
 function search(input) {
     if (input[0] === "N") {
         if (LineN.includes(input[1])) {
-            console.log("hllll")
             index = LineN.indexOf(input[1]);
             stops = stops + (index + 1);
             for (var i = 0; i <= index; i++) {
                 path.push(LineN[i]);
             } //call function here 
             lookupN(input);
-            //searchTwo(input);
         } else {
             index = LineN.indexOf(uS);
             stops = stops + (index + 1);
@@ -62,10 +62,14 @@ function search(input) {
 
     } else if (input[0] === "L") {
         if (LineL.includes(input[1])) {
+
             index = LineL.indexOf(input[1]);
             stops = stops + (index + 1);
+
+
             for (var il = 0; il <= index; il++) {
                 path.push(LineL[il]);
+
             }
             lookupL(input);
 
@@ -73,6 +77,7 @@ function search(input) {
         else {
             index = LineL.indexOf(uS);
             stops = stops + (index + 1);
+            console.log("lllll");
             for (lii = 0; lii <= index; lii++) {
                 path.push(LineL[lii]);
             }
@@ -84,76 +89,7 @@ function search(input) {
             for (var il = 0; il <= index; il++) {
                 path.push(LineSix[il]);
             }
-            function lookupL(input) {
-                if (LineL.includes(input[2])) {
-                    index = LineL.indexOf(input[1]);
-                    index2 = LineL.indexOf(input[2]);
-                    stops = stops + (index2 - index);
-                    for (var i = index + 1; i <= index2; i++) {
-                        path.push(LineL[i]);
-
-                    }
-                } else if (input[2] === "N") {
-                    if (LineN.includes(input[3])) {
-                        index = LineL.indexOf(input[1]);
-                        index2 = LineL.indexOf(uS);
-
-                        if (index > index2) {
-                            stops = stops + (index - index2);
-                        } else { stops = stops + (index2 - index); }
-                        for (var i = index + 1; i <= index2; i++) {
-                            path.push(LineL[i]);
-
-                        }
-                        index2 = LineN.indexOf(uS);
-                        index = LineN.indexOf(input[3]);
-                        if (LineL.indexOf(uS) > LineL.indexOf(input[1])) {
-
-                            stops = stops + (index2 - index);
-
-                            for (var i = index2 - 1; i >= index; i--) {
-                                path.push(LineN[i]);
-
-                            }
-                        } else {
-                            for (var i = index2; i >= index; i--) {
-                                path.push(LineN[i]);
-                            }
-                            stops = stops + (index2 - index);
-                        }
-
-                    }
-                } else if (input[2] === "6" || input[2] === "six") {
-                    if (LineSix.includes(input[3])) {
-                        index = LineL.indexOf(input[1]);
-                        index2 = LineL.indexOf(uS);
-                        if (index > index2) {
-                            stops = stops + (index - index2);
-                        } else { stops = stops + (index2 - index); }
-                        for (var i = index + 1; i <= index2; i++) {
-                            path.push(LineL[i]);
-
-                        }
-                        index2 = LineSix.indexOf(uS);
-                        index = LineSix.indexOf(input[3]);
-                        if (LineL.indexOf(uS) > LineL.indexOf(input[1])) {
-
-                            stops = stops + (index2 - index);
-
-                            for (var i = index2 - 1; i >= index; i--) {
-                                path.push(LineSix[i]);
-
-                            }
-                        } else {
-                            for (var i = index2; i >= index; i--) {
-                                path.push(LineSix[i]);
-                            }
-                            stops = stops + (index2 - index);
-                        }
-
-                    }
-                }
-            } lookupSix(input);
+            lookupSix(input);
         } else {
             index = LineSix.indexOf(uS);
             stops = stops + (index + 1);
@@ -245,16 +181,19 @@ function lookupL(input) {
         stops = stops + (index2 - index);
         for (var i = index + 1; i <= index2; i++) {
             path.push(LineL[i]);
-
         }
     } else if (input[2] === "N") {
+
         if (LineN.includes(input[3])) {
+
             index = LineL.indexOf(input[1]);
             index2 = LineL.indexOf(uS);
 
             if (index > index2) {
                 stops = stops + (index - index2);
+
             } else { stops = stops + (index2 - index); }
+
             for (var i = index + 1; i <= index2; i++) {
                 path.push(LineL[i]);
 
@@ -275,7 +214,7 @@ function lookupL(input) {
                 }
                 stops = stops + (index2 - index);
             }
-
+            console.log(stops);
         }
     } else if (input[2] === "6" || input[2] === "six") {
         if (LineSix.includes(input[3])) {
@@ -379,5 +318,4 @@ function lookupSix(input) {
     }
 }
 
-//search(Trip);
 

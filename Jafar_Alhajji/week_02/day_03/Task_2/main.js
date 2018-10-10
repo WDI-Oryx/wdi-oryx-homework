85b8cfa620a2d7ae3c3f831db6553909582f97c2
Jafar Abdullah <jafar@Jafars-MacBook-Air.local>
2018-10-10 08:45:46 +0300

    homowork for week2 day3 v2

--- Jafar_Alhajji/week_02/day_03/Task_2/main.js
+++ Jafar_Alhajji/week_02/day_03/Task_2/main.js
@@ -1,4 +1,4 @@
-console.log("bfgvv");
+
 
 var button = document.querySelector("#randomize");
 var inputW = document.querySelector(".width");
@@ -14,8 +14,8 @@ function addPhoto()
         var w= inputW.value;
         var h= inputH.value;
         var img = document.createElement("img");
-
-        img.setAttribute("src", "https://source.unsplash.com/"+w+"x"+h);
+        var text = "https://source.unsplash.com/"+w+"x"+h
+        img.setAttribute("src", text);
         document.body.appendChild(img);
 
         

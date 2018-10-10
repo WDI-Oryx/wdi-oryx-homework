1755c4f1830a05f3fa1e45fa6b8196d10101b833
leanaAlq <leana.a.q@gmail.com>
2018-10-10 21:24:26 +0300

    fix conflict

--- Leana_Alqurashi/week_02/day_04/starter1.js
+++ Leana_Alqurashi/week_02/day_04/starter1.js
@@ -1,3 +1,26 @@
-console.log('starter1.js linked');
+/*
+When the mouse enters any of the zones change its background color to green
+When the mouse leaves a zone change its background color black
+When you click on a zone it locks its color to green.
+*/
+$(document).ready(function () {
+    //code goes here
+    //1. When the mouse enters any of the zones change its background color to green
+    $(".zone").on("mouseenter", function () {
+        $(this).css("background-color", "green ");
+    })
+    //When the mouse leaves a zone change its background color black
+    $(".zone").on("mouseleave", function () {
+        $(this).css("background-color", "black");
+    })
 
-//code goes here
+    //3.
+    var $divzone = $(".zone");
+    $divzone.click(function () {
+
+        $(this).css("background-color", "green ");
+
+    })
+
+
+})

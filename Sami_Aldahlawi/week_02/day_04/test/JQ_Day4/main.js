48a706c197177610c2e324a8c32c525cf9bef02e
wh0-1-am <o.t5@hotmail.com>
2018-10-10 17:09:09 +0300

    w2d4 inclass

--- Sami_Aldahlawi/week_02/day_04/test/JQ_Day4/main.js
+++ Sami_Aldahlawi/week_02/day_04/test/JQ_Day4/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     console.log("JQ connected");
-    var $body = $("body");
-    var $newBox = $("<div/>").addClass("box").text("HEllO, My name Mreuoo");
-    $body.append($newBox);
-    $(".container").append($newBox);
-    console.log($newBox);
-
-
+    
 })
+
+
+function generate(arrayStr){
+
+}
+
 

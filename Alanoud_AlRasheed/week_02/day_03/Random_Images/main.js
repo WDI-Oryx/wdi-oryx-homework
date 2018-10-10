7e7f54c0f0d2ab422098ace572d19ce000a5775e
alanoudssr <alanoudssr@gmail.com>
2018-10-10 10:19:22 +0300

    HW W02-D03 + Bonus

--- Alanoud_AlRasheed/week_02/day_03/Random_Images/main.js
+++ Alanoud_AlRasheed/week_02/day_03/Random_Images/main.js
@@ -1,7 +1,15 @@
 
+
 function createImage() {
-  var newImg= document.createElement('img');
-  newImg.setAttribute('src','https://source.unsplash.com/random');
+
+  var newImg = document.createElement('img');
+  var userWidth = document.querySelector('.width');
+  var imgWidth = userWidth.value;
+  var userHeight = document.querySelector('.height');
+  var imgHeight = userHeight.value;
+  var userSite = document.querySelector('select');
+  var imgSite = userSite.options[userSite.selectedIndex].value;
+  newImg.setAttribute('src', imgSite + imgWidth.toString() + '/' + imgHeight.toString());
   var imgDiv = document.querySelector(".image");
   imgDiv.appendChild(newImg);
 }

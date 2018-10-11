b5fdb993ca5d23a83d985fb7cab2c06b91d643a8
thekraalowais <thekra_alowais@hotmail.com>
2018-10-11 09:12:07 +0300

    HW day 4

--- Thekra_Alowais/week_02/day_04/jQuery-event-HW/starter1.js
+++ Thekra_Alowais/week_02/day_04/jQuery-event-HW/starter1.js
@@ -1,3 +1,29 @@
 console.log('starter1.js linked');
 
-//code goes here
+//code goes here game 1
+$(function () {
+    var $theBox = $('.zone');
+    var $main = $('.container');
+    $theBox.on('mouseenter', function () {
+        console.log("mouse enter");
+        $(this).css('background-color', 'green');
+    })
+    $theBox.on('mouseleave', function () {
+        console.log("mouse leave");
+        $(this).css('background-color', '');
+
+    })
+    $theBox.on('click', function () {
+        console.log("clicked");
+        $(this).css('background-color', 'green');
+        //Remove event Listener
+        $(this).off('mouseleave');
+
+    })
+    // debugger;
+    //rgba(0,255,0,0)
+    // if ($('.zone').css('background-color') === 'green') {
+    //     console.log('congrats');
+    // }
+    // console.log($main.find('*').css());
+})
--- Thekra_Alowais/week_02/day_04/jQuery-event-HW/starter2.js
+++ Thekra_Alowais/week_02/day_04/jQuery-event-HW/starter2.js
@@ -1,3 +1,59 @@
 console.log('starter2.js linked');
 
-//code goes here
+//code goes here game 2
+
+
+
+var array = ['zone-2', 'zone-1', 'zone-3', 'zone-4'];
+var $theBox = $('.zone');
+
+$theBox.on('mouseenter', function () {
+    var boxIndex = $(this).index();
+    var boxId = $(this).attr('id');
+    var current = array.indexOf(boxId);
+    // for (var i = 0; i < array.length; i++) {
+    //     if (boxId === array[0]) {
+    //         console.log(boxId + " " + array[0]);
+    //         $(this).css('background-color', 'green');
+            
+
+    //     } else if (boxId === array[1]) {
+    //         console.log(boxId + " " + array[1]);
+    //         $(this).css('background-color', 'green');
+            
+    //     } else if (boxId === array[2]) {
+    //         console.log(boxId + " " + array[2]);
+    //         $(this).css('background-color', 'green');
+            
+    //     }else if (boxId === array[3]) {
+    //         console.log(boxId + " " + array[1]);
+    //         $(this).css('background-color', 'green');
+            
+    //     }
+    // }
+})
+$theBox.on('mouseleave', function () {
+    console.log("mouse leave");
+    $(this).css('background-color', '');
+
+})
+
+$theBox.on('click', function () {
+    var boxId = $(this).attr('id');
+    for (var i = 0; i < array.length; i++) {
+        // debugger;
+        if (boxId === array[i]) {
+            console.log(boxId + " test " );
+            $(this).css('background-color', 'green');
+            $(this).off('mouseleave');
+            // console.log(i);
+
+        }
+    }
+
+})
+
+
+
+
+

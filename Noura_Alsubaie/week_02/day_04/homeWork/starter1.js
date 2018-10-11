c2f459dc4a2cc124458fa958eaf4122495519eeb
NouraFL <nour3_@hotmail.com>
2018-10-11 15:12:11 +0300

    homework

---
+++ Noura_Alsubaie/week_02/day_04/homeWork/starter1.js
@@ -0,0 +1,16 @@
+
+$(function(){
+
+$('.zone').on('mouseover', function(event){
+     $(this).css('background', 'green');
+$(this).on('mouseleave', function(event){
+    $(this).css('background', '');
+})
+
+$('.zone').click(function(event){
+    $(this).off();
+    $(this).css('background', 'green');
+});
+
+})
+});

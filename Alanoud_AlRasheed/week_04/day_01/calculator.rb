8b061e2d95dbe59f94a7adafaafb968b37bd2689
alanoudssr <alanoudssr@gmail.com>
2018-10-21 21:39:04 +0300

    HW_W04_D01

---
+++ Alanoud_AlRasheed/week_04/day_01/calculator.rb
@@ -0,0 +1,43 @@
+require 'rainbow'
+
+p "Welcome to the calculator!"
+
+def prompt
+    puts Rainbow("Which operation would you like to perform? \n(*) Multipliction \n(/) Division \n(-) Subtraction \n(+) Addition \n(pow) Power of \n(q) Quit").yellow
+    operation = gets.chomp
+end 
+
+choice = prompt()
+
+until choice == "q"
+
+    def calulator(x =0, y=0, op)
+        case op
+        when "*"
+            return "#{x} #{op} #{y} = #{x*y}" 
+        when "+"
+            return "#{x} #{op} #{y} = #{x+y}" 
+        when "/"
+            return "#{x} #{op} #{y} = #{x/y}" 
+        when "-"
+            return "#{x} #{op} #{y} = #{x-y}" 
+        when "pow"
+            return "#{x} #{op} #{y} = #{x ** y}" 
+        else 
+            "You didn't choose a correct opertator"
+        end 
+    end 
+
+    p "Now enter the value of x:"
+    x = gets.chomp.to_f
+
+    p "Now enter the value of y:"
+    y = gets.chomp.to_f
+
+    p calulator(x,y, choice)
+
+    choice = prompt()
+
+end 
+
+p "Thanks for using the calculator."

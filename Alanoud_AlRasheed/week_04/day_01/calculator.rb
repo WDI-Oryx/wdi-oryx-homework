8df29e34458e904030a51f849a762851cfdd8d7f
alanoudssr <alanoudssr@gmail.com>
2018-10-21 21:52:10 +0300

    HW_W04_D01

--- Alanoud_AlRasheed/week_04/day_01/calculator.rb
+++ Alanoud_AlRasheed/week_04/day_01/calculator.rb
@@ -3,7 +3,7 @@ require 'rainbow'
 p "Welcome to the calculator!"
 
 def prompt
-    puts Rainbow("Which operation would you like to perform? \n(*) Multipliction \n(/) Division \n(-) Subtraction \n(+) Addition \n(pow) Power of \n(q) Quit").yellow
+    puts Rainbow("Which operation would you like to perform? \n(*) Multipliction \n(/) Division \n(-) Subtraction \n(+) Addition \n(e) Exponentiation \n(s) Square Root \n(q) Quit").yellow
     operation = gets.chomp
 end 
 
@@ -21,8 +21,10 @@ until choice == "q"
             return "#{x} #{op} #{y} = #{x/y}" 
         when "-"
             return "#{x} #{op} #{y} = #{x-y}" 
-        when "pow"
+        when "e"
             return "#{x} #{op} #{y} = #{x ** y}" 
+        when "s"
+            return "The Square Root of #{x} is #{Math.sqrt(x)}, and the Square Root of #{y} is #{Math.sqrt(y)}" 
         else 
             "You didn't choose a correct opertator"
         end 

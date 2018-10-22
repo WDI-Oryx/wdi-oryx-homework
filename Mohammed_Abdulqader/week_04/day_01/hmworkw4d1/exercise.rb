c65c588269f73a2154412272e2de77d902764dad
triple0m <mrmohmmedmustafa@hotmail.com>
2018-10-22 08:01:18 +0300

    hw-w4d1

--- Mohammed_Abdulqader/week_04/day_01/hmworkw4d1/exercise.rb
+++ Mohammed_Abdulqader/week_04/day_01/hmworkw4d1/exercise.rb
@@ -8,6 +8,9 @@ def menu
   puts "(s) Subtraction"
   puts "(m) Multiplication"
   puts "(d) Devesion"
+  puts "(p) power"
+  puts "(sq) square roots"
+  puts "(B) BMI"
 
   puts "(q) Quit"
   puts ""
@@ -63,6 +66,42 @@ result = first_num / second_num
 puts "#{first_num} / #{second_num} = #{result} "
 end
 
+def powerNumber
+puts ""
+puts "What is your first number ?"
+first_num = gets.chop.to_f
+
+puts "what is your second number ?"
+second_num = gets.chomp.to_f
+result = (first_num) ** (second_num)
+puts "#{first_num} power #{second_num} = #{result} "
+end
+
+def  squareRoots
+puts ""
+puts "What is your first number ?"
+first_num = gets.chop.to_f
+
+puts "what is your second number ?"
+second_num = gets.chomp.to_f
+result = (first_num + second_num / first_num )/2
+puts "#{first_num}  square roots #{second_num} = #{result} "
+end
+
+
+def BMI
+puts ""
+puts "What is your  height ?"
+first_num = gets.chop.to_f
+
+puts "what is your weight ?"
+second_num = gets.chomp.to_f
+result = (first_num / second_num ** 2)
+puts " height #{first_num}  weihgt #{second_num} BMI = #{result} "
+end
+
+
+
 
 user_choice = menu()
 
@@ -75,6 +114,13 @@ until user_choice == "q"
   when "m" then mul()
     
   when "d" then dev()
+
+  when "p" then powerNumber()
+
+
+   when "sq" then squareRoots()
+    
+   when "B" then BMI()
   
 end
 

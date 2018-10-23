9bdba0b2c9bbb75a8885badcda80a26b1884d9b8
Abdullah-AlQahtani <abdullahq.ksa@gmail.com>
2018-10-24 00:32:06 +0300

    Homework for week 4 day 3

--- Abdullah_AlQahtani/week_04/day_03/classes/main.rb
+++ Abdullah_AlQahtani/week_04/day_03/classes/main.rb
@@ -1,64 +1,36 @@
-# class Bank
-#     attr_accessor :name , :balance
-#     def initialize(name)
-#         @balance = 0
-#         @name = name
-#     end
-
-# end
-
-# class BankAccount < Bank
-#     def deposit(amount)
-#         amount = amount + @balance
-#         puts "#{@name} Balance: #{amount}"
-#     end
-
-#     def withdraw(amount)
-#            if amount < @balance 
-#             remaining  = @balance -  amount
-#             puts " You just withdraw #{amount}"
-#             puts " Your Balance now is: #{remaining}"
-#            else
-#             puts " You want to withdraw: #{amount} and you have in your balance: #{@balance}"
-#            end
-#            if remaining < 100
-#             puts " Low balance, please add funds."
-#            end
-#     end
-
-# end
-
-# account = BankAccount.new("Falah")
-# account.deposit(101)
-# account.withdraw(100)
-
 class Bank
-    attr_accessor :owner, :account
-   
-    def initialize(name, balance = 0)
-      @ownwer = name
-      @balance = balance
-      @account = []
+    attr_accessor :name , :balance
+    def initialize(name)
+        @balance = 0
+        @name = name
     end
-   
-   
-    def create_bank_account()
-      puts "Enter your name"
-      @owner = gets.chomp
-      puts "Enter your balance"
-      @balance = gets.chomp
-      @account.push(@owner, @balance)
+
+end
+
+class BankAccount < Bank
+    def deposit(amount)
+        @balance = amount + @balance
+        puts "#{@name} Balance: #{@balance}"
     end
-   
-    #   def total_balance
-   
-    #   end
-   
-    #   def delete_account
-    #   end
-   end
-   
-   bank = Bank.new("Razan")
-   bank.create_bank_account()
-   p @account
+
+    def withdraw(amount)
+           if amount < @balance 
+            remaining  = @balance -  amount
+            puts " You just withdraw #{amount}"
+            puts " Your Balance now is: #{remaining}"
+           else
+            puts " You want to withdraw: #{amount} and you have in your balance: #{@balance}"
+           end
+           if remaining <= 100
+            puts " Low balance, please add funds."
+           else
+            puts " You are ok"
+           end
+    end
+
+end
+
+account = BankAccount.new("Falah")
+account.deposit(300)
+account.withdraw(100)
 

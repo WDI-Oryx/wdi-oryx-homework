ad24cd8dee98bd30494e82eaf7282a5c8c6a6371
thekraalowais <thekra_alowais@hotmail.com>
2018-10-28 09:00:44 +0300

    HW w4 d5

---
+++ Thekra_Alowais/week_04/day_05/games_on_rails/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #Rock, Paper, Scissors game
+  get "games/rps/:input", to: "games#rps"
+  get "games/magic/:question", to: "games#question"
+  # resources :secrets
+end

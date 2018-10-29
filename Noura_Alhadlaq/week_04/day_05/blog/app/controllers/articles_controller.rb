a1f1077624ee403617f2784c3d7a4a1aa38ecc5e
NouraAlhadlaq <noura6.1994@gmail.com>
2018-10-29 10:42:43 +0300

    rails project

--- Noura_Alhadlaq/week_04/day_05/blog/app/controllers/articles_controller.rb
+++ Noura_Alhadlaq/week_04/day_05/blog/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
@@ -33,6 +35,13 @@ class ArticlesController < ApplicationController
     else
       render "edit"
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
--- Noura_Alhadlaq/week_04/day_05/blog/config/routes.rb
+++ Noura_Alhadlaq/week_04/day_05/blog/config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   get "welcome/index"
 
   resources :articles
 
   root "welcome#index"
+
+  resources :articles do
+    resources :comments
+  end
+
+  
 end

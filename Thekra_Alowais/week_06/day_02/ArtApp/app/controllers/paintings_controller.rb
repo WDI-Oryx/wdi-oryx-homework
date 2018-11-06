f2931106397aa99b7b29941469766c0bb2d01ce8
thekraalowais <thekra_alowais@hotmail.com>
2018-11-06 08:35:34 +0300

    HW w6 d2

---
+++ Thekra_Alowais/week_06/day_02/ArtApp/app/controllers/paintings_controller.rb
@@ -0,0 +1,27 @@
+class PaintingsController < ApplicationController
+  def index
+    @paintings = Painting.all
+  end
+
+  def show
+    @painting = Painting.find_by_id(params[:id])
+  end
+
+  def new
+    @painting = Painting.new
+  end
+
+  def edit
+  end
+
+  def create
+    painting = Painting.create(painting_params)
+    redirect_to paintings_path
+  end
+
+  private
+
+  def painting_params
+    params.require(:painting).permit(:name, :description, :artist_id, category_ids: [])
+  end
+end

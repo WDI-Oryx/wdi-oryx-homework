bc3d9ae59bd65e954ef9d33ecb8f6223a954c14a
alanoudssr <alanoudssr@gmail.com>
2018-11-18 11:21:47 +0300

    HW_W07_D05

--- Alanoud_AlRasheed/week_07/day_05/api/main.js
+++ Alanoud_AlRasheed/week_07/day_05/api/main.js
@@ -1,17 +1,21 @@
-const apiURL = "http://api.open-notify.org/iss-now";
+const apiURL = "https://rickandmortyapi.com/api/character/1,2,3,4,5";
 const method = "GET";
 const dataType = "JSON";
 
 fetch(apiURL).then(function (response) {
     return response.json();
 }).then(function (data) {
-    const position = data.iss_position;
+    const characters = data;
     document.body.innerHTML += `
-        <h1>International Space Station Current Location</h1>
+        <h1>Rick's Family</h1>
         `;
-    document.body.innerHTML += `
-        <h3>Longitude: ${position.longitude}</h3>
-        <p>Latitude: ${position.latitude}</p>
+    for (let i in characters) {
+        document.body.innerHTML += `
+        <div style="float:left; margin:3px;">
+        <h3>Name: ${characters[i].name}</h3>
+        <img src=" ${characters[i].image}">
+        </div>
         `;
+    }
 });
 

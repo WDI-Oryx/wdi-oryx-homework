a1b76bb6df47abc754ae37c3c297f55fff0ae08c
Moath <mfalthaw@ucalgary.ca>
2018-11-26 19:17:22 +0300

    Revert "hw week9_day1"

--- Razan_Almusa/week_08/day_02/MessageApp/db/seeds.rb
+++
@@ -1,7 +0,0 @@
-Message.destroy_all
-
-Message.create(content: "First msg")
-Message.create(content: "Second msg")
-Message.create(content: "Third Message")
-Message.create(content: "Fourth Message")
-Message.create(content: "Fifth msg")

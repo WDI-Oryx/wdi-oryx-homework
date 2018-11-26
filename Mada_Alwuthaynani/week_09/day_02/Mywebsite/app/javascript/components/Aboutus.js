0cd8b1726a3ffc352bf63441efb9737a5316c482
madaawadh <madaawadh1@gmail.com>
2018-11-26 20:54:57 +0300

    hw w9 d2

---
+++ Mada_Alwuthaynani/week_09/day_02/Mywebsite/app/javascript/components/Aboutus.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Aboutus extends React.Component {
+  render () {
+    return (
+      <div>
+      <h1>this is about us</h1>
+    </div>
+    );
+  }
+}
+
+export default Aboutus

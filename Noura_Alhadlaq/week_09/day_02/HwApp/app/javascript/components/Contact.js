1c48cb1ec2726775f62b8c89234088608f85083d
NouraAlhadlaq <noura6.1994@gmail.com>
2018-11-27 10:28:58 +0300

    w_09 d_02 hw

---
+++ Noura_Alhadlaq/week_09/day_02/HwApp/app/javascript/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Contact extends React.Component {
+	render() {
+		return (
+			<div>
+				<h2>Contact: </h2>
+				<h4>Email: Nora@gmail.com</h4>
+			</div>
+		);
+	}
+}
+
+export default Contact;

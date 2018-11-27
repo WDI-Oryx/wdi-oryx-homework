72772a542f136ab902ac4e5e6e9d396856b76a48
Halima <Halimaalshahrani@gmail.com>
2018-11-27 23:49:22 +0300

    Homework_d2w9

---
+++ Halimah_Alshahrani/week_09/day_02/Homework_d2w9/HalimaApp/app/javascript/components/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+class Contact extends React.Component {
+  render() {
+    return (
+    
+      <div>
+        <h2>Contact: </h2>
+        <br/>
+        <a href="https://www.linkedin.com/in/halimaalshahrani/"> Linkedin </a> <br/>
+        <a href="https://twitter.com/__Halima?lang=ar"> Twitter: @__Halima</a> <br/>
+        <a href="mailto:Halimaalshahrani@gmail.com">E-mail: Halimaalshahrani@gmail.com </a>
+       
+      </div>
+    
+    
+    );
+  }
+}
+export default Contact;

9b36bacefd828e56bcf993625c484da8996d814f
NjodA <njod@njods-MacBook-Air.local>
2018-11-27 08:30:30 +0300

    w9d2

---
+++ Njod_Alnasser/week_09/day_02/mywebApp/myWebApp/app/javascript/components/Contact.js
@@ -0,0 +1,16 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Contact extends React.Component {
+  render () {
+    return (
+      <div>
+<ul>
+  <li> Mobile Number : 0554327512</li>
+  <li> Email : njod@gmail.com</li>
+  </ul>    
+    </div>
+    );
+  }
+}
+
+export default Contact;
---
+++ Njod_Alnasser/week_09/day_02/mywebApp/myWebApp/app/javascript/components/Routes.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { HashRouter, Route, Link } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+import Contact from "./Contact";
+class Routes extends React.Component {
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <nav>
+            <Link to="/"> Home</Link>
+            <Link to="/about"> About</Link>
+            <Link to="/contact"> Contact</Link>
+          </nav>
+
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+        </div>
+      </HashRouter>
+    );
+  }
+}
+
+export default Routes;

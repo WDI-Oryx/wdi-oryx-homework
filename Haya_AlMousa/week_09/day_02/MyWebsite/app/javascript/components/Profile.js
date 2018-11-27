80e192816e798835c1f32e5de9344f16c6d9ba1d
HayaMAM <hayamousa0088@gmail.com>
2018-11-27 17:46:11 +0300

    homework of week 9 day 2

---
+++ Haya_AlMousa/week_09/day_02/MyWebsite/app/javascript/components/Profile.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Profile extends React.Component {
+  render () {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Profile
